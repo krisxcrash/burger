4cf3256beab0936b738f777bf49c2cb7b0a1de23
Kristine Martin <kristinemartin@kristines-mbp.attlocal.net>
2017-09-19 21:48:59 -0700

    updated main.handlebars and basic set up of schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_DB;
+USE burgers_DB;
+
+CREATE TABLE burgers (
+    id INT NOT NULL auto_increment,
+    burger_name VARCHAR (30) NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT 0,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
