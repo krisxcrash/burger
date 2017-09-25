15a53b8ffe656663ac3b6099b0a6d25545fa1dff
Kristine Martin <kristinemartin@kristines-mbp.attlocal.net>
2017-09-24 18:01:00 -0700

    added JAWS_DB connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 //MySQL Connection set up
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "uci.projects",
-    password: "uciprojects123",
-    database: "burgers_DB"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    conntection= mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "uci.projects",
+        password: "uciprojects123",
+        database: "burgers_DB"
+    });
+};
 
 //Make Connection
 connection.connect(function(err){
